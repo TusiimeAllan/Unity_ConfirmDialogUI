ddb9748a0bb23612f6434f9cd8a0755b9ee6c8d3
Tusiime Allan <allantusiime@gmail.com>
2024-06-28 02:27:06 +0300

    Project Setup and Upgrading Asset

---
+++ Assets/Dialog UI/Scripts/DialogButtonColor.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyUI.Dialogs {
+
+	[System.Serializable]
+	public enum DialogButtonColor {
+		Black,
+		Purple,
+		Magenta,
+		Blue,
+		Green,
+		Yellow,
+		Orange,
+		Red,
+		Gray
+	}
+
+}
